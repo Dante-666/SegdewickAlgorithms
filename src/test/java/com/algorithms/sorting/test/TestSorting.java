fa2420c0cc8e27b6a8d5711d7382ef601f239f7a
Dante-666 <j.singh.logan@gmail.com>
2014-06-11 22:41:09 +0530

    Add shuffling in TestSorting.java

--- src/test/java/com/algorithms/sorting/test/TestSorting.java
+++ src/test/java/com/algorithms/sorting/test/TestSorting.java
@@ -7,13 +7,30 @@ import org.testng.annotations.Test;
 @Test(groups = "Test-Sorting")
 public class TestSorting {
 
+        public int[] sortDataGenerator(int seed){
+
+	        int[] array=new int[seed];
+		int rand;
+		int temp;
+		for(int i = 0 ; i < seed - 1 ; i++){
+		        array[i]=i;
+		}
+		for(int i = 0 ; i < seed - 1 ; i++){
+		        rand = (int) Math.random() * (seed - 1);
+			temp=array[i];
+			array[i]=array[rand];
+			array[rand]=temp;
+		}
+		return array;
+	}
+        @Test
 	public void testBubbleSort() {
-		//Path file = FileSystems.getDefault().getPath("/src/main/resources", "testGraph.txt"); //"testGraph.txt";
 		
-	        int[] array={7,8,9,3,4,65,2,541,3,2,25,67,5,55,45,545};
+	        int[] array=sortDataGenerator(1000);
 		
 		try {
 		BubbleSort testBubble= new BubbleSort(array);
+		System.out.println(testBubble.toString());
 		testBubble.sortArray();
 		System.out.println(testBubble.toString());
 		}
